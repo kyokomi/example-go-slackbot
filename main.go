834577c65211bd2eb9d33ec09ab920c530465ce3
kyokomi <kyoko1220adword@gmail.com>
2017-02-05 17:39:40 +0900

    Add esa plugin

--- main.go
+++ main.go
@@ -12,13 +12,22 @@ import (
 	"github.com/kyokomi/slackbot/plugins/akari"
 	"github.com/kyokomi/slackbot/plugins/cron/v2"
 	"github.com/kyokomi/slackbot/plugins/docomo"
+	"github.com/kyokomi/slackbot/plugins/esa"
 	"github.com/kyokomi/slackbot/plugins/kohaimage"
 	"github.com/kyokomi/slackbot/plugins/lgtm"
 	"github.com/kyokomi/slackbot/plugins/naruhodo"
+	"github.com/kyokomi/slackbot/plugins/router"
 	"github.com/kyokomi/slackbot/plugins/suddendeath"
 	"github.com/kyokomi/slackbot/plugins/sysstd"
-	"github.com/kyokomi/slackbot/plugins/router"
 )
+
+type Config struct {
+	redisToGoURL string
+	slackToken   string
+	docomoAPIKey string
+	esaTeam      string
+	esaToken     string
+}
 
 func init() {
 	if fl := log.Flags(); fl&log.Ltime != 0 {
@@ -26,9 +35,9 @@ func init() {
 	}
 }
 
-func setupSlackBot(redisToGoURL, slackToken, docomoAPIKey string) error {
+func setupSlackBot(cfg Config) error {
 	// setup repository
-	addr, password, err := parseRedisURL(redisToGoURL)
+	addr, password, err := parseRedisURL(cfg.redisToGoURL)
 	if err != nil {
 		return err
 	}
@@ -41,7 +50,7 @@ func setupSlackBot(redisToGoURL, slackToken, docomoAPIKey string) error {
 		return err
 	}
 
-	botCtx, err := slackbot.NewBotContextNotSysstd(slackToken)
+	botCtx, err := slackbot.NewBotContextNotSysstd(cfg.slackToken)
 	if err != nil {
 		return err
 	}
@@ -52,27 +61,39 @@ func setupSlackBot(redisToGoURL, slackToken, docomoAPIKey string) error {
 	botCtx.AddPlugin("sysstd", sysPlugin)
 	botCtx.AddPlugin("cron", cron.NewPlugin(cronCtx))
 	botCtx.AddPlugin("router", router.NewPlugin(botCtx.Client, docomoRepository))
-	if docomoAPIKey != "" {
-		botCtx.AddPlugin("docomo", docomo.NewPlugin(godocomo.NewClient(docomoAPIKey), docomoRepository))
+	if cfg.docomoAPIKey != "" {
+		botCtx.AddPlugin("docomo", docomo.NewPlugin(godocomo.NewClient(cfg.docomoAPIKey), docomoRepository))
 	}
 	botCtx.AddPlugin("akari", akari.NewPlugin())
 	botCtx.AddPlugin("naruhodo", naruhodo.NewPlugin())
 	botCtx.AddPlugin("lgtm", lgtm.NewPlugin())
 	botCtx.AddPlugin("koha", kohaimage.NewPlugin(kohaimage.NewKohaAPI()))
 	botCtx.AddPlugin("suddendeath", suddendeath.NewPlugin())
+	if cfg.esaTeam != "" && cfg.esaToken != "" {
+		botCtx.AddPlugin("esa", esa.NewPlugin(cfg.esaTeam, cfg.esaToken))
+	}
 
 	botCtx.WebSocketRTM()
 	return nil
 }
 
 func main() {
-	var serverAddr, slackToken, dApiKey string
+	var serverAddr, slackToken, dApiKey, esaTeam, esaToken string
 	flag.StringVar(&serverAddr, "addr", ":8080", "serverのaddr")
 	flag.StringVar(&slackToken, "slackToken", os.Getenv("SLACK_BOT_TOKEN"), "SlackのBotToken")
 	flag.StringVar(&dApiKey, "docomo", os.Getenv("DOCOMO_APIKEY"), "DocomoのAPIKEY")
+	flag.StringVar(&esaTeam, "esa-team", os.Getenv("ESA_TEAM"), "esaのチーム名")
+	flag.StringVar(&esaToken, "esa-token", os.Getenv("ESA_TOKEN"), "esaのToken")
 	flag.Parse()
 
-	if err := setupSlackBot(os.Getenv("REDISTOGO_URL"), slackToken, dApiKey); err != nil {
+	cfg := Config{
+		redisToGoURL: os.Getenv("REDISTOGO_URL"),
+		slackToken:   slackToken,
+		docomoAPIKey: dApiKey,
+		esaTeam:      esaTeam,
+		esaToken:     esaToken,
+	}
+	if err := setupSlackBot(cfg); err != nil {
 		log.Fatalln(err)
 	}
 
